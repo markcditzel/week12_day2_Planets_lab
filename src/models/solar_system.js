b27f1fc9f312079d981f08531e01d52ce9c18fb3
Raphael Ugha <raphaelugha@gmail.com>
2018-09-11 12:44:38 +0100

    Created views. Added configuration to pub_sub.js. Publish all planets

--- src/models/solar_system.js
+++ src/models/solar_system.js
@@ -1,5 +1,17 @@
+const PubSub = require('../helpers/pub_sub.js');
+
 const SolarSystem = function(planets) {
   this.planets = planets;
+
 };
+
+
+SolarSystem.prototype.bindEvents = function(){
+  PubSub.publish('SolarSystem:all-planets-ready', this.planets);
+
+
+}
+
+
 
 module.exports = SolarSystem;
